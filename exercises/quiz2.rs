e6f3f432d8e8e75c43d3dc7212909d5f3fbcc069
Weichao-Wang12345 <2639840196@qq.com>
2025-03-29 22:30:22 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,13 +32,25 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: Vec<String,Command>) -> Vec<String>{
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String>{
         // TODO: Complete the output declaration!
         let mut output:Vec<String>= vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
             match command {
-               
+                Command::Uppercase=>{
+                    output.push(string.to_uppercase().to_string());
+                }
+                Command::Trim=>{
+                    output.push(string.trim().to_string());
+                }
+                Command::Append(n)=>{
+                    let mut ans=string.to_string();
+                    for i in 0..*n{
+                        ans= ans +"bar";
+                    }
+                    output.push(ans);
+                }
             }
         }
         output
